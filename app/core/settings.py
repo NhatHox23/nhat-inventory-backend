a183e53e3a687e9efc2e814ddb2c074881c56268
NhatHox23 <88417626+NhatHox23@users.noreply.github.com>
2021-12-17 10:02:39 +0700

    Test git action flake8 fail (#1)
    * Test git action flake8 fail
    
    * Fix Flake8 issues
    
    * Fix Typo
    
    * Fix another typo
    
    * Fixing missing app name
    
    * Add build Docker Compose

--- app/core/settings.py
+++ app/core/settings.py
@@ -19,7 +19,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-&!t%($pf@3f2hk#iu8+@q2+e!ov+i)4+nv$qd)!+nx=6w+hti8'
+SECRET_KEY = 'django-insecure-&!t%($pf@3f2hk#iu8+@q2+e!ov+i)4+nv$qd)!+nx=6w' \
+             '+hti8 '
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -82,16 +83,20 @@ DATABASES = {
 
 AUTH_PASSWORD_VALIDATORS = [
     {
-        'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.UserAttributeSimilarityValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.MinimumLengthValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.CommonPasswordValidator',
     },
     {
-        'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+        'NAME': 'django.contrib.auth.password_validation'
+                '.NumericPasswordValidator',
     },
 ]
 
