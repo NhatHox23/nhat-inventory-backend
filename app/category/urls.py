f8c32a4ae05dff626bb44a0517c61e552057fce9
NhatHo <nhat.ho@go2joy.vn>
2021-12-24 10:30:00 +0700

    Remove cateogry

--- app/category/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import CategoryApiViewSet
-
-app_name = 'category'
-
-urlpatterns = [
-    path('list/', CategoryApiViewSet.as_view({'get': 'list'}),
-         name="list-category")
-]
